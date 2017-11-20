2d918d6eba6c6408355a03c7f29c40aa4527870a
axefx <axellfx@gmail.com>
2017-11-20 15:47:28 -0800

    BookListing component to hold my book reviews

---
+++ src/components/BookListing/BookListing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import BookHolder from "../BookHolder/BookHolder";
+
+
+class BookListing extends React.Component {
+  render() {
+    return (
+      <div className="book-list-container">
+        <BookHolder
+          bookTitle="Pragmatic Guide to Sass 3"
+          coverUrl="https://images-na.ssl-images-amazon.com/images/I/41lcZTsmEeL._SX331_BO1,204,203,200_.jpg"
+          bookDesc="book description goes here..."
+          link="https://www.amazon.com/Pragmatic-Guide-Sass-Modern-Style/dp/1680501763/ref=sr_1_1?ie=UTF8&qid=1511047039&sr=8-1&keywords=pragmatic+guide+to+sass+3&dpID=41lcZTsmEeL&preST=_SY344_BO1,204,203,200_QL70_&dpSrc=srch"
+        />
+        <BookHolder
+          bookTitle="Book 2"
+          coverUrl="https://images-na.ssl-images-amazon.com/images/I/41lcZTsmEeL._SX331_BO1,204,203,200_.jpg"
+          bookDesc="book description goes here..."
+          link="https://www.amazon.com/Pragmatic-Guide-Sass-Modern-Style/dp/1680501763/ref=sr_1_1?ie=UTF8&qid=1511047039&sr=8-1&keywords=pragmatic+guide+to+sass+3&dpID=41lcZTsmEeL&preST=_SY344_BO1,204,203,200_QL70_&dpSrc=srch"
+        />
+        <p>Book3</p>
+      </div>
+    );
+  }
+}
+
+export default BookListing;
