74036ada07ee4b642553bbd828749e91b51da524
Axel <axacorro@gmail.com>
2022-07-19 19:47:24 -0700

    includes userAvatar propety with link

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -14,7 +14,7 @@ module.exports = {
   userName: "Axel", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Los Angeles, California", // User location to display in the author segment.
-  // userAvatar: "https://api.adorable.io/avatars/150/test.png",  User avatar to display in the author segment.
+  userAvatar: "https://avatars.githubusercontent.com/u/17439087?v=4", // User avatar to display in the author segment.
   // userDescription:
   //  "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
@@ -22,15 +22,15 @@ module.exports = {
     {
       label: "GitHub",
       url: "https://github.com/axefx",
-      iconClassName: "fa fa-github"
+      iconClassName: "fa fa-github",
     },
     {
       label: "Email",
       url: "mailto:axellfx@gmail.com",
-      iconClassName: "fa fa-envelope"
-    }
+      iconClassName: "fa fa-envelope",
+    },
   ],
   copyright: "Copyright © 2017. axeFx.github.io", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
 };
