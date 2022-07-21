ca198b6233b83cc7f58f062907f0d65f04038e91
Axel <axacorro@gmail.com>
2022-07-20 19:24:57 -0700

    refactor postlisting component

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -1,38 +1,24 @@
 import React from "react";
 import PostPreview from "./PostPreview";
 
-class PostListing extends React.Component {
-  getPostList() {
-    const postList = [];
-    this.props.postEdges.forEach(postEdge => {
-      postList.push({
-        path: postEdge.node.fields.slug,
-        tags: postEdge.node.frontmatter.tags,
-        cover: postEdge.node.frontmatter.cover,
-        title: postEdge.node.frontmatter.title,
-        date: postEdge.node.frontmatter.date,
-        excerpt: postEdge.node.excerpt,
-        timeToRead: postEdge.node.timeToRead
-      });
-    });
-    return postList;
-  }
-  render() {
-    const postList = this.getPostList();
-    return (
-      <div className="md-grid md-grid--no-spacing md-cell--middle">
-        <div
-          id="recentPosts"
-          className="md-background--card md-grid md-cell--8 mobile-fix"
-        >
-          <p className="md-display-2">Recent Posts</p>
-          {postList.map(post => (
-            <PostPreview key={post.title} postInfo={post} />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+const PostListing = (props) => {
+  const postList = props.postEdges.map((postEdge) => ({
+    path: postEdge.node.fields.slug,
+    tags: postEdge.node.frontmatter.tags,
+    cover: postEdge.node.frontmatter.cover,
+    title: postEdge.node.frontmatter.title,
+    date: postEdge.node.frontmatter.date,
+    excerpt: postEdge.node.excerpt,
+    timeToRead: postEdge.node.timeToRead,
+  }));
+  return (
+    <div id="postListingContainer">
+      <h2>Recent Posts</h2>
+      {postList.map((post) => (
+        <PostPreview key={post.title} postInfo={post} />
+      ))}
+    </div>
+  );
+};
 
 export default PostListing;
