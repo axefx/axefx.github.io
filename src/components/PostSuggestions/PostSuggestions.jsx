e4ee248bb9c6a29130a4e5c8678bd93813dc2478
Axel <axacorro@gmail.com>
2022-07-28 19:03:34 -0700

    removes fonticon components

--- src/components/PostSuggestions/PostSuggestions.jsx
+++ src/components/PostSuggestions/PostSuggestions.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import FontIcon from "react-md/lib/FontIcons";
 import Link from "gatsby-link";
 import "./PostSuggestions.scss";
 
@@ -10,13 +9,6 @@ export default class PostSuggestions extends Component {
     return (
       <div className="post-suggestions md-grid md-cell--12">
         <Link to={postFields.prevSlug} className="post-suggestion">
-          <FontIcon
-            forceFontSize
-            forceSize="64"
-            className="secondary-color arrow-nav"
-          >
-            arrow_back
-          </FontIcon>
           <div className="headline-container hide-on-mobile">
             <h2 className="md-body-2 secondary-color">Previous</h2>
             <h6 className="md-headline secondary-color">
@@ -31,13 +23,6 @@ export default class PostSuggestions extends Component {
               {postFields.nextTitle}
             </h6>
           </div>
-          <FontIcon
-            forceFontSize
-            forceSize="64"
-            className="secondary-color arrow-nav"
-          >
-            arrow_forward
-          </FontIcon>
         </Link>
       </div>
     );
