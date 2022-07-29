96e2fa5efc773027c481f145f254fbb3e8f00572
Axel <axacorro@gmail.com>
2022-07-28 19:17:37 -0700

    updates about page to use mainlayout component

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import Helmet from "react-helmet";
 import About from "../components/About/About";
 import config from "../../data/SiteConfig";
+import MainLayout from "../components/Layout";
 
 class AboutPage extends Component {
   render() {
     return (
-      <div className="about-container">
+      <>
         <Helmet>
           <title>{`About | ${config.siteTitle}`}</title>
           <link rel="canonical" href={`${config.siteUrl}/about/`} />
         </Helmet>
-        <About />
-      </div>
+        <MainLayout location={this.props.location} className="about-container">
+          <About />
+        </MainLayout>
+      </>
     );
   }
 }
