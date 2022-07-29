b8c0f682d8d744f091ddabd9f49d83dd648a2656
Axel <axacorro@gmail.com>
2022-07-28 19:15:42 -0700

    updates to use custom components

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
-import Card from "react-md/lib/Cards/Card";
-import CardTitle from "react-md/lib/Cards/CardTitle";
-import CardText from "react-md/lib/Cards/CardText";
-import Avatar from "react-md/lib/Avatars";
-import FontIcon from "react-md/lib/FontIcons";
-import IconSeparator from "react-md/lib/Helpers/IconSeparator";
 import { Follow } from "react-twitter-widgets";
 import UserLinks from "../UserLinks/UserLinks";
 import "./UserInfo.scss";
+import Avatar from "../Avatar";
+import Card, { CardTitle, CardContent } from "../Card";
 
 class UserInfo extends Component {
   render() {
@@ -17,7 +13,7 @@ class UserInfo extends Component {
       userLocation,
       userDescription,
       userLinks,
-      userTwitter
+      userTwitter,
     } = this.props.config;
     const { expanded } = this.props;
     const userLinksElement = (
@@ -40,24 +36,21 @@ class UserInfo extends Component {
           avatar={userAvatar && <Avatar src={userAvatar} role="presentation" />}
           title={userName && userName}
           subtitle={
-            userTwitter
-              ? <Follow
-                  username={userTwitter}
-                  options={{ count: expanded ? "none" : "none" }}
-                />
-              : "Author"
+            userTwitter ? (
+              <Follow
+                username={userTwitter}
+                options={{ count: expanded ? "none" : "none" }}
+              />
+            ) : (
+              "Author"
+            )
           }
         />
-        <CardText expandable={!expanded}>
-          {userLocation &&
-            <IconSeparator label={userLocation} iconBefore>
-              <FontIcon iconClassName="fa fa-map-marker" />
-            </IconSeparator>}
-          <p>
-            {userDescription && userDescription}
-          </p>
+        <CardContent expandable={!expanded}>
+          {userLocation}
+          <p>{userDescription && userDescription}</p>
           {userLinksElement}
-        </CardText>
+        </CardContent>
       </Card>
     );
   }
--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,34 +1,30 @@
 import React, { Component } from "react";
-import Button from "react-md/lib/Buttons";
+import Button from "../Button";
 import "./UserLinks.scss";
 
 class UserLinks extends Component {
   getLinkElements() {
     const { userLinks } = this.props.config;
     const { labeled } = this.props;
-    return userLinks.map(link =>
+    return userLinks.map((link) => (
       <Button
         icon={!labeled}
         flat={labeled}
-        secondary
+        theme="secondary"
         key={link.label}
         iconClassName={link.iconClassName}
         href={link.url}
       >
         {labeled ? link.label : ""}
       </Button>
-    );
+    ));
   }
   render() {
     const { userLinks } = this.props.config;
     if (!userLinks) {
       return null;
     }
-    return (
-      <div className="user-links">
-        {this.getLinkElements()}
-      </div>
-    );
+    return <div className="user-links">{this.getLinkElements()}</div>;
   }
 }
 
