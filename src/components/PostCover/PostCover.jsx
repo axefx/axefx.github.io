9b388d4154f43de16387c9646f8521575c2c3681
Axel <axacorro@gmail.com>
2022-08-02 22:37:40 -0700

    removes mobile prop and updates static coverHeight

--- src/components/PostCover/PostCover.jsx
+++ src/components/PostCover/PostCover.jsx
@@ -3,13 +3,13 @@ import "./PostCover.scss";
 
 class PostCover extends Component {
   render() {
-    const { postNode, mobile } = this.props;
+    const { postNode } = this.props;
     const post = postNode.frontmatter;
     /* eslint no-undef: "off"*/
     const cover = post.cover.startsWith("/")
       ? __PATH_PREFIX__ + post.cover
       : post.cover;
-    const coverHeight = mobile ? 180 : 350;
+    const coverHeight = 180;
     return (
       <div
         style={{ backgroundImage: `url(${cover})`, height: `${coverHeight}px` }}
